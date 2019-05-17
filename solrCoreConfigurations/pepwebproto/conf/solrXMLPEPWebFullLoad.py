84caffda8a1f431916fb8876cf562c70243f259b
Neil Shapiro <nrshapiro@gmail.com>
2019-05-17 01:12:18 -0400

    Various updates and a few fixes to the ingestion program including cmd line arguments, logging, and better documentation with TODOs.  In the fulldocs schema art_kwds had to be changed to multivalue=true.

--- solrCoreConfigurations/pepwebproto/conf/solrXMLPEPWebFullLoad.py
+++ solrCoreConfigurations/pepwebproto/conf/solrXMLPEPWebFullLoad.py
@@ -16,22 +16,37 @@ Todo:
 
 Initial Release: Neil R. Shapiro 2019-05-15 (python 2.7)
 
+Revisions:
+    2019-05-16: Addded command line options to specify a different path for PEPSourceInfo.json
+                Added error logging using python's built-in logging library default INFO level
+
+
 """
 # Disable many annoying pylint messages, warning me about variable naming for example.
 # yes, in my Solr code I'm caught between two worlds of snake_case and camelCase.
 
-# pylint: disable=C0321,C0103,C0301,E1101,C0303
+# pylint: disable=C0321,C0103,C0301,E1101,C0303,E1004
+
 import re
 import os
 import time
 import sys
+import logging
+from datetime import datetime
+from optparse import OptionParser
 
 # If on Python 2.X
 #from __future__ import print_function
 import json
 import pysolr
 from lxml import etree
-from optparse import OptionParser
+
+#TODO: Move common support functions (and potentially all of common interest) to separate include script (library)
+class ExitOnExceptionHandler(logging.StreamHandler):
+    def emit(self, record):
+        super().emit(record)
+        if record.levelno in (logging.ERROR, logging.CRITICAL):
+            raise SystemExit(-1)
 
 def elementsToStrings(elementNode, xPathDef):
     """
@@ -130,6 +145,7 @@ def readSourceInfoDB(journalInfoFile):
 
     return retVal
 
+
 def main():
     """
     PEP Full Text Data loader
@@ -139,38 +155,73 @@ def main():
              Load articles, extract references from the bibliography, and
              add to Solr database
 
-             2019-05-15 - Working, first version 0.1.15 for first version schema - nrs
+             Use -h option to see command line options and version number
         """
 
-    parser = OptionParser(usage="%prog [options] - PEP Solr Full Text Data Loader", version="%prog ver. 0.1.15")
-    parser.add_option("-u", "--url",
-                      dest="solrURL", default="http://localhost:8983/solr/",
-                      help="Base URL of Solr api (without core)", metavar="URL")
+    scriptSourcePath = os.path.dirname(os.path.realpath(__file__))
+    programNameShort = "OPASXMLFullLoad"
+
+    parser = OptionParser(usage="%prog [options] - PEP Solr Full Text Data Loader", version="%prog ver. 0.1.16")
+    parser.add_option("-c", "--corename", dest="coreName", default="pepwebproto",
+                      help="the Solr corename (holding the collection) to connect to, i.e., where to send data, e.g., 'pepwebproto'")
+    parser.add_option("-d", "--dataroot", dest="rootFolder", default=r"C:\solr-8.0.0\server\solr\pepwebproto\sampledata\_PEPA1",
+                      help="Root folder path where input data is located")
+    parser.add_option("-l", "--loglevel", dest="logLevel", default=logging.INFO,
+                      help="Level at which events should be logged")
     parser.add_option("-r", "--resetcoredata",
                       action="store_true", dest="resetCoreData", default=False,
                       help="reset the data in the collection/core")
-    parser.add_option("-d", "--dataroot", dest="rootFolder", default=r"C:\solr-8.0.0\server\solr\pepwebproto\sampledata\_PEPA1",
-                      help="Root folder where input data is located")
-    parser.add_option("-c", "--corename", dest="coreName", default="pepwebproto",
-                      help="the Solr corename (holding the collection) to connect to, i.e., where to send data")
+    parser.add_option("-s", "--sourcodedb", dest="sourceCodeDBPath", default=None,
+                      help="Folder where PEPSourceInfo.json is located (simple source info DB in JSON)")
+    parser.add_option("-u", "--url",
+                      dest="solrURL", default="http://localhost:8983/solr/",
+                      help="Base URL of Solr api (without core)", metavar="URL")
+
+    # TODO: later; even odd starting page numbers selectivity will allow us to run two instances at a time, and the processes will work on different files, for shorter process times.
+    #parser.add_option("-e", "--even", dest="evenFiles", default=0,
+                      #help="Only do files where the starting page is even")
+    #parser.add_option("-o", "--odd", dest="evenFiles", default=0,
+                      #help="Only do files where the starting page is odd")
 
 
     (options, args) = parser.parse_args()
 
+    logFilename = programNameShort + "_" + datetime.today().strftime('%Y-%m-%d') + ".log"
+    processingErrorCount = 0
+    processingWarningCount = 0
+    processedFilesCount = 0
+    logging.basicConfig(handlers=[ExitOnExceptionHandler()], filename=logFilename, level=options.logLevel)
+    logger = logging.getLogger(programNameShort)
+    logger.info('Started at %s', datetime.today().strftime('%Y-%m-%d %H:%M:%S"'))
+
     solrAPIURL = options.solrURL + options.coreName  # e.g., http://localhost:8983/solr/pepwebproto'
+
+    if options.sourceCodeDBPath is None:
+        options.sourceCodeDBPath = options.rootFolder
 
     print "Input data Root: ", options.rootFolder
     print "Solr Core: ", options.coreName
     print "Reset Core Data: ", options.resetCoreData
     print "Solr solrAPIURL: ", solrAPIURL
+    print "Logfile: ", logFilename
 
     time_start = time.time()
     # get a list of all the XML files
 
     # import data about the PEP codes for journals and books.
     #  Codes are like APA, PAH, ... and special codes like ZBK000 for a particular book
-    pepSourceCodeDBName = r'\PEPSourceInfo.json'
-    sourceInfoDB = readSourceInfoDB(options.rootFolder + pepSourceCodeDBName)
+    pepSourceCodeDBName = r'PEPSourceInfo.json'
+    sourceInfoDB = {}
+    try:
+        # param specified path, or by default, same as database
+        sourceInfoDB = readSourceInfoDB(os.path.join(options.sourceCodeDBPath, pepSourceCodeDBName))
+    except IOError:
+        # try where the script is running from instead.
+        logger.info("%s not found in %s. Trying base installation path.", pepSourceCodeDBName, options.sourceCodeDBPath)
+        try:
+            sourceInfoDB = readSourceInfoDB(os.path.join(scriptSourcePath, pepSourceCodeDBName))
+        except IOError:
+            logger.fatal("%s not found in %s.", pepSourceCodeDBName, scriptSourcePath)
 
     # Setup a Solr instance. The timeout is optional.
     solr = pysolr.Solr(solrAPIURL, timeout=1)
@@ -181,23 +232,22 @@ def main():
 
     # find all processed XML files where build is (bEXP_ARCH1) in path
     # glob.glob doesn't unfortunately work to do this in Py2.7.x
-    pat = r"(.*)\(bEXP_ARCH1\)\.(xml|XML)"
+    pat = r"(.*)\(bEXP_ARCH1\)\.(xml|XML)$"
     filePatternMatch = re.compile(pat)
     filenames = []
+    countFiles = 0
     for root, d_names, f_names in os.walk(options.rootFolder):
         for f in f_names:
+            #TODO: Check file date against last import date for that file, and if not newer, skip
+            #TODO: If newer, write filename to a database, maybe along with art_id, and file mod date/time so it can be skipped next time if not modified.
+            #TODO: We also need a "rebuild" option in the command line, to bypass the date skipping (but not the recording)
+
             if filePatternMatch.match(f):
+                countFiles += 1
                 filenames.append(os.path.join(root, f))
 
-    print "Ready to import full-text records from %s files at path: %s" % (len(filenames), options.rootFolder)
+    print "Ready to import full-text records from %s files at path: %s" % (countFiles, options.rootFolder)
     bibTotalReferenceCount = 0
-
-    #----------------------------------------------------------------------------------------
-    #Data Cases - quick testing for a specific structure, just uncomment filenames and set filename there
-    #Test File: Example Dream
-    #filenames = [r"C:\solr-8.0.0\server\solr\pepwebproto\sampledata\_PEPA1\_PEPArchive\APA\063.2015\APA.063D.0769A(bEXP_ARCH1).XML"]
-    #----------------------------------------------------------------------------------------
-
     for n in filenames:
         f = open(n)
         fileXMLContents = f.read()
@@ -223,9 +273,11 @@ def main():
             artIDFromFile = artIDFromFile.upper()
 
             if artIDFromFile != art_id:
-                print "Warning: File name ID tagged and artID disagree.  %s vs %s" % (art_id, artIDFromFile)
+                logger.warn("File name ID tagged and artID disagree.  %s vs %s", art_id, artIDFromFile)
+                processingWarningCount += 1
         except Exception, err:
-            print "Warning: issue reading file's article id."
+            logger.warn("Issue reading file's article id. (%s)", err)
+            processingWarningCount += 1
 
         # Article data
         #<!-- Common fields -->
@@ -241,11 +293,12 @@ def main():
             art_pepsourcetitleabbr = ""
             art_pepsourcetitlefull = ""
             art_pepsourcetype = ""
-            print "Error: PEP Source Code %s not found in db (%s).  Use the 'PEPSourceInfo export' after fixing the issn table in MySQL DB" % (art_pepsrccode, options.rootFolder + pepSourceCodeDBName)
+            logger.warn("Error: PEP Source Code %s not found in db (%s).  Use the 'PEPSourceInfo export' after fixing the issn table in MySQL DB", art_pepsrccode, options.rootFolder + pepSourceCodeDBName)
 
         except Exception, err:
-            print err
-            sys.exit(-1)
+            logger.error("Error: Problem with this files source info. File skipped. (%s)", err)
+            processingErrorCount += 1
+            continue
 
         #art_vol = pepxml.xpath('//artinfo/artvol/node()')
         #art_year = pepxml.xpath('//artinfo/artyear/node()')
@@ -357,71 +410,91 @@ def main():
         # this build of the schema now has all XML data fields indicated in the field name
         # the snake_case works better for Solr; as a result, a bit of it thus creeps into this python code, but
         # I still prefer to use camelCase.  Unfortunately, lint is unhappy I use both :)
-        response_update = solr.add([
-            {
-                "id": art_id,                   # important: note this is unique id for every reference
-                "art_id": art_id,
-                "art_title_xml": elementsToStrings(pepxml, "//arttitle"),
-                "art_subtitle_xml": elementsToStrings(pepxml, "//artsubtitle"),
-                "title": title,
-                "art_pepsrccode": art_pepsrccode,
-                "art_pepsourcetitleabbr": art_pepsourcetitleabbr,
-                "art_pepsourcetitlefull": art_pepsourcetitlefull,
-                "art_pepsourcetype": art_pepsourcetype,
-                "art_authors": art_authors,
-                "art_authors_unlisted": pepxml.xpath('//artinfo/artauth/aut[@listed="false"]/@authindexid'),
-                "art_authors_xml": elementsToStrings(pepxml, "//aut"),
-                "art_year": pepxml.xpath('//artinfo/artyear/node()'),
-                "art_vol": getSingleSubnodeText(pepxml, 'artvol'),
-                "art_pgrg": getSingleSubnodeText(pepxml, "artpgrg"),
-                "art_iss": pepxml.xpath('//artissue/node()'),
-                "art_doi": pepxml.xpath("//artinfo/@doi"),
-                "art_lang": art_lang,                       # list
-                "art_issn": pepxml.xpath('//artinfo/@ISSN'),
-                "art_origrx": pepxml.xpath('//artinfo/@origrx'),
-                "art_qual": pepxml.xpath("//artinfo/artqual/@rx"),
-                "art_kwds": pepxml.xpath("//artinfo/artkwds/node()"),
-                "art_type": pepxml.xpath("//artinfo/@arttype"),
-                "art_newsecnm": pepxml.xpath("//artinfo/@newsecnm/node()"),
-                "art_citeas_xml": art_citeas,
-                "bib_entries_json":bib_refentries_struct,  # hmm, I wonder if we should type suffix other fields?
-                "authors": art_authors,         # for common names convenience
-                "author_bio_xml": elementsToStrings(pepxml, "//nbio"),
-                "author_aff_xml": elementsToStrings(pepxml, "//autaff"),
-                "bk_title_xml": elementsToStrings(pepxml, "//artbkinfo/bktitle"),
-                "bk_alsoknownas_xml": elementsToStrings(pepxml, "//artbkinfo/bkalsoknownas"),
-                "bk_editors_xml": elementsToStrings(pepxml, "//bkeditors"),
-                "bk_seriestitle_xml": elementsToStrings(pepxml, "//bkeditors"),
-                "bk_pubyear": pepxml.xpath("//bkpubyear/node()"),
-                "caption_text_xml": elementsToStrings(pepxml, "//caption"),
-                "caption_title_xml": elementsToStrings(pepxml, "//ctitle"),
-                "headings_xml": headings,
-                "references_xml": elementsToStrings(pepxml, "/pepkbd3//be"),
-                "abstracts_xml": elementsToStrings(pepxml, "///abs"),
-                "summaries_xml": elementsToStrings(pepxml, "///summaries"),
-                "terms_xml": elementsToStrings(pepxml, "//impx[@type='TERM2']"),
-                "terms_highlighted_xml": elementsToStrings(pepxml, "//b") + elementsToStrings(pepxml, "//i"),
-                "dialogs_spkr": pepxml.xpath("//dialog/spkr/node()"),
-                "dialogs_xml": elementsToStrings(pepxml, "//dialog"),
-                "dreams_xml": elementsToStrings(pepxml, "//dream"),
-                "notes_xml": elementsToStrings(pepxml, "//note"),
-                "panels_spkr": elementsToStrings(pepxml, "//panel/spkr"),
-                "panels_xml": elementsToStrings(pepxml, "//panel"),
-                "poems_src": pepxml.xpath("//poem/src/node()"),
-                "poems_xml": elementsToStrings(pepxml, "//poem"),
-                "quotes_spkr": pepxml.xpath("//quote/spkr/node()"),
-                "quotes_xml": elementsToStrings(pepxml, "//quote"),
-                "meta_xml": elementsToStrings(pepxml, "//meta"),
-                "text_xml": fileXMLContents,
-            }])
-        # print response_update
+
+        processedFilesCount += 1
+
+        try:
+            response_update = solr.add([
+                {
+                    "id": art_id,                   # important: note this is unique id for every reference
+                    "art_id": art_id,
+                    "art_title_xml": elementsToStrings(pepxml, "//arttitle"),
+                    "art_subtitle_xml": elementsToStrings(pepxml, "//artsubtitle"),
+                    "title": title,
+                    "art_pepsrccode": art_pepsrccode,
+                    "art_pepsourcetitleabbr": art_pepsourcetitleabbr,
+                    "art_pepsourcetitlefull": art_pepsourcetitlefull,
+                    "art_pepsourcetype": art_pepsourcetype,
+                    "art_authors": art_authors,
+                    "art_authors_unlisted": pepxml.xpath('//artinfo/artauth/aut[@listed="false"]/@authindexid'),
+                    "art_authors_xml": elementsToStrings(pepxml, "//aut"),
+                    "art_year": pepxml.xpath('//artinfo/artyear/node()'),
+                    "art_vol": getSingleSubnodeText(pepxml, 'artvol'),
+                    "art_pgrg": getSingleSubnodeText(pepxml, "artpgrg"),
+                    "art_iss": pepxml.xpath('//artissue/node()'),
+                    "art_doi": pepxml.xpath("//artinfo/@doi"),
+                    "art_lang": art_lang,                       # list
+                    "art_issn": pepxml.xpath('//artinfo/@ISSN'),
+                    "art_origrx": pepxml.xpath('//artinfo/@origrx'),
+                    "art_qual": pepxml.xpath("//artinfo/artqual/@rx"),
+                    "art_kwds": pepxml.xpath("//artinfo/artkwds/node()"),
+                    "art_type": pepxml.xpath("//artinfo/@arttype"),
+                    "art_newsecnm": pepxml.xpath("//artinfo/@newsecnm/node()"),
+                    "art_citeas_xml": art_citeas,
+                    "bib_entries_json":bib_refentries_struct,  # hmm, I wonder if we should type suffix other fields?
+                    "authors": art_authors,         # for common names convenience
+                    "author_bio_xml": elementsToStrings(pepxml, "//nbio"),
+                    "author_aff_xml": elementsToStrings(pepxml, "//autaff"),
+                    "bk_title_xml": elementsToStrings(pepxml, "//artbkinfo/bktitle"),
+                    "bk_alsoknownas_xml": elementsToStrings(pepxml, "//artbkinfo/bkalsoknownas"),
+                    "bk_editors_xml": elementsToStrings(pepxml, "//bkeditors"),
+                    "bk_seriestitle_xml": elementsToStrings(pepxml, "//bkeditors"),
+                    "bk_pubyear": pepxml.xpath("//bkpubyear/node()"),
+                    "caption_text_xml": elementsToStrings(pepxml, "//caption"),
+                    "caption_title_xml": elementsToStrings(pepxml, "//ctitle"),
+                    "headings_xml": headings,
+                    "references_xml": elementsToStrings(pepxml, "/pepkbd3//be"),
+                    "abstracts_xml": elementsToStrings(pepxml, "///abs"),
+                    "summaries_xml": elementsToStrings(pepxml, "///summaries"),
+                    "terms_xml": elementsToStrings(pepxml, "//impx[@type='TERM2']"),
+                    "terms_highlighted_xml": elementsToStrings(pepxml, "//b") + elementsToStrings(pepxml, "//i"),
+                    "dialogs_spkr": pepxml.xpath("//dialog/spkr/node()"),
+                    "dialogs_xml": elementsToStrings(pepxml, "//dialog"),
+                    "dreams_xml": elementsToStrings(pepxml, "//dream"),
+                    "notes_xml": elementsToStrings(pepxml, "//note"),
+                    "panels_spkr": elementsToStrings(pepxml, "//panel/spkr"),
+                    "panels_xml": elementsToStrings(pepxml, "//panel"),
+                    "poems_src": pepxml.xpath("//poem/src/node()"),
+                    "poems_xml": elementsToStrings(pepxml, "//poem"),
+                    "quotes_spkr": pepxml.xpath("//quote/spkr/node()"),
+                    "quotes_xml": elementsToStrings(pepxml, "//quote"),
+                    "meta_xml": elementsToStrings(pepxml, "//meta"),
+                    "text_xml": fileXMLContents,
+                }])
+        except Exception, err:
+            processingErrorCount += 1
+            logger.error("Solr call exception %s", err)
+
+
+            # print response_update
         f.close()
+
+        if processingErrorCount > 900:
+            print "Processed File Count: %s" % processedFilesCount
+            logger.exception("File processing error count > 100.  Stopping data load.")
+
 
     solr.commit()
     time_end = time.time()
 
-    print "Finished! Imported %s documents with %s references. Elapsed time: %s secs" % (len(filenames), bibTotalReferenceCount, time_end-time_start)
+    msg = "Finished! Imported %s documents with %s references. Elapsed time: %s secs" % (len(filenames), bibTotalReferenceCount, time_end-time_start)
+    print msg
+    logger.info(msg)
+    if processingWarningCount + processingErrorCount > 0:
+        print "  Issues found.  Warnings: %s, Errors: %s.  See log file %s" % (processingWarningCount, processingErrorCount, logFilename)
 
 # -------------------------------------------------------------------------------------------------------
 # run it!
-main()
+
+if __name__ == "__main__":
+    main()
