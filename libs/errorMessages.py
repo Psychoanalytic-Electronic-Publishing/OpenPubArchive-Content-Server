d4f2009564dfe8d1609ba91565fdb016a8915607
Neil Shapiro <nrshapiro@gmail.com>
2019-07-14 14:41:43 -0400

    addendum

---
+++ libs/errorMessages.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Error messages
+ERR_CREDENTIALS = "Credentials"
+ERR_CONDITIONS = "Conditions"
+ERR_MSG_RECOVERABLE_CONDITION = "A recoverable error on the server has occurred"
+ERR_MSG_LOGIN_UNSUPPORTED = "Not enough information to log in."
+ERR_MSG_LOGIN_CREDENTIALS = "Credentials incorrect, or not found."
+ERR_MSG_INSUFFICIENT_INFO = "Not enough information for operation"
+ERR_MSG_LOGOUT_UNSUPPORTED = "There's no session to log out from."
+ERR_MSG_DEPRECATED = "This endpoint is deprecated."
+
+
