5ce639854ddd7c07394c1be223eb8808f4abefa8
nrshapiro <nrshapiro@gmail.com>
2019-12-04 16:04:46 -0500

    Update localsecrets_fillin_and_change_name_to_localsecrets.py
    Added new 'constant' to localsecrets.py template which is called localsecrets_fillin_and_change_name_to_localsecrets.py (i.e., instructions are in file name!) for CORS.  Added default values for some constants if they don't need to be set specifically for the paltform config.

--- app/config/localsecrets_fillin_and_change_name_to_localsecrets.py
+++ app/config/localsecrets_fillin_and_change_name_to_localsecrets.py
@@ -6,13 +6,20 @@ url_pads="http://www.psychoanalystdatabase.com/PEPProductLive/PEPProduct.asmx"
 
 import hashlib, binascii, os
 
+# use this to set the active configuration below
 CONFIG = "AWSCodesyper"
-# defaults
-DEBUG_DOCUMENTS = 1
-DBPORT = 3306 # default
-API_PORT_MAIN = 9100
-SSH_HOST = None
 
+# Template for local settings, including sensitive info
+# These are the defaults, if not overridden below
+DEBUG_DOCUMENTS = 1
+DBPORT = 3306
+API_PORT_MAIN = 9100
+SSH_HOST = None # if set, ssh tunnel is active for database
+CORS_REGEX = "(.*\.)?(pep\-web|development)\..*"
+API_BINARY_IMAGE_SOURCE_PATH = r"./images" # where external images in articles will be found
+API_PDF_ORIGINALS_PATH = r"./pdforiginal" # where pdf originals for documents are found for the download feature
+
+# setting config above selects the active configuration
 if CONFIG == "Local":
     API_PORT_MAIN = 9100
     COOKIE_DOMAIN = ".development.org"
@@ -26,7 +33,8 @@ if CONFIG == "Local":
     DBNAME = "opascentral"
     API_BINARY_IMAGE_SOURCE_PATH = r"fill-me-in"
     API_PDF_ORIGINALS_PATH = r"fill-me-in"
-
+    CORS_REGEX = "(.*\.)?(pep\-web|development)\..*"
+    
 elif CONFIG == "AWSCodesyper": # Codesypher setup, but running from my PC
     API_PORT_MAIN = 9100
     COOKIE_DOMAIN = ".pep-web.rocks" # OR pep-web.rocks when running all from AWS
@@ -42,4 +50,27 @@ elif CONFIG == "AWSCodesyper": # Codesypher setup, but running from my PC
     SSH_HOST = None
     API_BINARY_IMAGE_SOURCE_PATH = r"fill-me-in"
     API_PDF_ORIGINALS_PATH = r"fill-me-in"
+    CORS_REGEX = "(.*\.)?(pep\-web|development)\..*"
 
+elif CONFIG == "TunneledRemoteExample":
+    import paramiko
+    from paramiko import SSHClient
+    import sshtunnel # the tunneling is to the db server, not solr
+    API_PORT_MAIN = 9100
+    # domain running server
+    COOKIE_DOMAIN = ".development.org" # . means include any subdomains
+    BASEURL = "development.org"
+    SOLRURL = "http://3.99.999.999/solr/" # EXAMPLE Solr 
+    SOLRUSER = "user"                     # need user and password for codesypher server
+    SOLRPW = "fill-me-in"
+    DBPORT = 3306
+    DBHOST = "localhost" # with tunneling, you are on localhost
+    DBUSER = "root"
+    DBPW = "fill-me-in"
+    DBNAME = "opascentral"
+    # if SSH_HOST is not none, then DB will be tunneled to
+    SSH_HOST = 'ec2-99-999-999-9.compute-1.amazonaws.com' # made up AWS example
+    SSH_USER = "fill-me-in"
+    SSH_PORT = 22
+    PKEYFILE = "fill-me-in"
+    SSH_MYPKEY = paramiko.RSAKey.from_private_key_file(PKEYFILE)
