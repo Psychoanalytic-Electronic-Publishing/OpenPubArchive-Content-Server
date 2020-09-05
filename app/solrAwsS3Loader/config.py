5693988e4c3e9b43b7fffd42a46db046b8e6a54e
nrshapiro <nrshapiro@gmail.com>
2020-09-05 09:28:09 -0400

    Append to last commit (files omitted)
    A few files that should have been in the last commit

---
+++ app/solrAwsS3Loader/config.py
@@ -0,0 +1,9 @@
+# Configuration file for solrXMLWebLoad
+
+# Global variables (for data and instances)
+bibTotalReferenceCount = 0
+options = None
+
+# constants
+COMMITLIMIT = 1000  # commit the load to Solr every X articles
+
