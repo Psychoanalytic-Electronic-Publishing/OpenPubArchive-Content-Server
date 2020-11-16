180e9521666875763a6de44c9ff17f919df236af
nrshapiro <nrshapiro@gmail.com>
2020-11-16 14:36:02 -0500

    2020.1116.1.Alpha
    Didn't save the main.py file before committing, thus the version number change was not included.

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@
 __author__      = "Neil R. Shapiro"
 __copyright__   = "Copyright 2020, Psychoanalytic Electronic Publishing"
 __license__     = "Apache 2.0"
-__version__     = "2020.1114.2.Alpha"
+__version__     = "2020.1116.1.Alpha"
 __status__      = "Development"
 
 """
