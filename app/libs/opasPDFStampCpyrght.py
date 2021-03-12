9d69efc87669bbb78d7ade69efb4fb8fb85f2f01
nrshapiro <nrshapiro@gmail.com>
2021-03-12 10:46:57 -0500

    2021.0312.1.Beta - Adjust Header/Footers on PDFOrig
    - Depending on page size, header placement is inconsistent (sometimes not shown)
    - Footer seems more reliable, so use both, the same, so at least one is on there!
    - All tests passed on local development databases

--- app/libs/opasPDFStampCpyrght.py
+++ app/libs/opasPDFStampCpyrght.py
@@ -37,9 +37,11 @@ class PDF(FPDF):
             # Position at 1.5 cm from bottom
             self.set_y(-15)
             # Arial italic 8
-            self.set_font('Helvetica', 'I', 4)
-            # Page number
-            self.cell(0, 10, f'Original Journal Reprint -- Downloaded from PEP-Web by {self.username_to_set}', 0, 0, 'C')
+            self.set_font('Helvetica', 'B', 6) # font size 6, B for bold
+            header_copyright = f"Copyrighted Material. For use only by {self.username_to_set}. Reproduction prohibited. Usage subject to PEP terms & conditions (see terms.pep-web.org)."
+            # use same header text as footer, depending on page size, more reliable in footer
+            self.cell(w=0, h=10, txt=header_copyright, border=0, ln=0, link="http://terms.pep-web.org")
+            # self.cell(0, 10, f'Original Journal Reprint -- Downloaded from PEP-Web by {self.username_to_set}', 0, 0, 'C')
         except Exception as e:
             logger.error(f"Cannot add Copyright Footer: {e}")                      
 
