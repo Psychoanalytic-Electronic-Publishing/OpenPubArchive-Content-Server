fbb235e09f027ea6c25717aca2ecd9f499754343
nrshapiro <nrshapiro@gmail.com>
2021-07-19 15:30:20 -0400

    2021.0719/v2.1.44
    Just an update to a test

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ __copyright__   = "Copyright 2019-2021, Psychoanalytic Electronic Publishing"
 __license__     = "Apache 2.0"
 # funny source things happening, may be crosslinked files in the project...watch this one
 
-__version__     = "2021.0719/v2.1.43" # semver versioning now added after date.
+__version__     = "2021.0719/v2.1.44" # semver versioning now added after date.
 __status__      = "Beta"
 
 """
