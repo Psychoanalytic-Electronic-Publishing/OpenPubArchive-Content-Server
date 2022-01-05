83e054d7c64388276fb9e2b4ad2e150a95e9a69c
nrshapiro <nrshapiro@gmail.com>
2022-01-05 10:19:01 -0500

    2022.0105/v2.1.110
    Title/Description string cleanup

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ __copyright__   = "Copyright 2019-2021, Psychoanalytic Electronic Publishing"
 __license__     = "Apache 2.0"
 # funny source things happening, may be crosslinked files in the project...watch this one
 
-__version__     = "2022.0104/v2.1.109" # semver versioning after date.
+__version__     = "2022.0105/v2.1.110" # semver versioning after date.
 __status__      = "Beta"
 
 """
