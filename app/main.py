ff9e653261dd7147b27734dbc27019cc90c7bdfe
nrshapiro <nrshapiro@gmail.com>
2022-08-29 07:42:55 -0400

    2022.0829/v2.1.173
    Updated 2022-08-29 Changed protobuf version req per ERROR in online process: mysql-connector-python 8.0.30 has requirement protobuf<=3.20.1,>=3.11.0, but you'll have protobuf 4.21.5 which is incompatible.

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@
 __author__      = "Neil R. Shapiro"
 __copyright__   = "Copyright 2019-2022, Psychoanalytic Electronic Publishing"
 __license__     = "Apache 2.0"
-__version__     = "2022.0826/v2.1.172"   # semver versioning after date.
+__version__     = "2022.0829/v2.1.173"   # semver versioning after date.
 __status__      = "Development/Libs/Loader"  
 
 """
