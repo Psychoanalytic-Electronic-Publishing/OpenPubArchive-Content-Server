b432222b700b90ec6c40b685185b6fb21abcee3f
Jordan Allen <jordan@jordanallen.org>
2023-05-08 18:29:14 +0100

    Provision ECR with lifecycle rule
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

---
+++ infra/staging/container.tf
@@ -0,0 +1,36 @@
+resource "aws_ecr_repository" "opas_repository" {
+  name                 = "opas"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "opas_policy" {
+  repository = aws_ecr_repository.opas_repository.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+output "ecr_repo_url" {
+  value = aws_ecr_repository.opas_repository.repository_url
+}
