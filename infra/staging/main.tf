c818ecbbfa6561169496e8afe017fe50ec1a7dc3
Jordan Allen <jordan@jordanallen.org>
2023-05-08 18:22:08 +0100

    Initialise Terraform
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

---
+++ infra/staging/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    key = "global/s3/opas-stage.tfstate"
+  }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
