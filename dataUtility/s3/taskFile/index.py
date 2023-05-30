b45a3744a78b7afc6a74e77beb65014d7daaaa5c
jordanallen-dev <79658146+jordanallen-dev@users.noreply.github.com>
2023-05-30 18:39:29 +0100

    Trigger data utility with s3 upload (#204)
    * Trigger lambda on JSON file upload
    
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>
    
    * Start state machine for each uploaded json file
    
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>
    
    * Execute state machine with contents of JSON file
    
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>
    
    * Delete JSON file after executing state machine
    
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>
    
    ---------
    
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

---
+++ dataUtility/s3/taskFile/index.py
@@ -0,0 +1,23 @@
+import boto3
+import json
+import os
+import uuid
+
+sf = boto3.client("stepfunctions")
+s3 = boto3.resource('s3')
+
+def handler(event, context):
+    for record in event["Records"]:
+        bucket = record["s3"]["bucket"]["name"]
+        key = record["s3"]["object"]["key"]
+
+        obj = s3.Object(bucket, key)
+        body = obj.get()['Body'].read().decode('utf-8')
+
+        sf_response = sf.start_execution(
+            stateMachineArn=os.environ["STATE_MACHINE_ARN"],
+            name=str(uuid.uuid4()),
+            input=json.dumps(json.loads(str(body))),
+        )
+
+        obj.delete()
---
+++ infra/modules/data-utility-s3/lambda.tf
@@ -0,0 +1,52 @@
+module "execute_task_file" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "4.9.0"
+
+  function_name           = "${var.stack_name}-execute-task-file-handler-${var.env}"
+  source_path             = "../../dataUtility/s3/taskFile"
+  handler                 = "index.handler"
+  runtime                 = "python3.8"
+  ignore_source_code_hash = true
+
+  environment_variables = {
+    STATE_MACHINE_ARN = var.state_machine_arn
+  }
+
+  tags = {
+    stage = var.env
+    stack = var.stack_name
+  }
+}
+
+resource "aws_lambda_permission" "allow_execute_task_file" {
+  statement_id  = "${var.stack_name}-allow-task-file-${var.env}"
+  action        = "lambda:InvokeFunction"
+  function_name = module.execute_task_file.lambda_function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = data.aws_s3_bucket.pep_web_live_data.arn
+}
+
+resource "aws_iam_role_policy" "start_task_lambda_policy" {
+  role = module.execute_task_file.lambda_role_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:DeleteObject"
+        ]
+        Effect   = "Allow"
+        Resource = "${data.aws_s3_bucket.pep_web_live_data.arn}/*"
+      },
+      {
+        Action = [
+          "states:StartExecution",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
