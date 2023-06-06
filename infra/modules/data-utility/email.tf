597209f0e39c26a6cd56bc59a841e5ff8f5b24c0
Jordan Allen <jordan@jordanallen.org>
2023-06-06 18:43:40 +0100

    Define startup email lambda
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/modules/data-utility/email.tf
+++ infra/modules/data-utility/email.tf
@@ -2,11 +2,11 @@ resource "aws_sns_topic" "status_updates" {
   name = "${var.stack_name}-status-updates-${var.env}"
 }
 
-module "send_status_email" {
+module "send_completion_email" {
   source  = "terraform-aws-modules/lambda/aws"
   version = "4.9.0"
 
-  function_name           = "${var.stack_name}-send-status-email-${var.env}"
+  function_name           = "${var.stack_name}-send-completion-email-${var.env}"
   source_path             = "../../dataUtility/email"
   handler                 = "finish.handler"
   runtime                 = "python3.8"
@@ -23,8 +23,8 @@ module "send_status_email" {
 }
 
 
-resource "aws_iam_role_policy" "send_status_email_lambda_policy" {
-  role = module.send_status_email.lambda_role_name
+resource "aws_iam_role_policy" "send_completion_email_lambda_policy" {
+  role = module.send_completion_email.lambda_role_name
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -46,3 +46,42 @@ resource "aws_iam_role_policy" "send_status_email_lambda_policy" {
     ]
   })
 }
+
+module "send_startup_email" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "4.9.0"
+
+  function_name           = "${var.stack_name}-send-startup-email-${var.env}"
+  source_path             = "../../dataUtility/email"
+  handler                 = "start.handler"
+  runtime                 = "python3.8"
+  ignore_source_code_hash = true
+
+  environment_variables = {
+    SNS_TOPIC_ARN = aws_sns_topic.status_updates.arn
+  }
+
+  tags = {
+    stage = var.env
+    stack = var.stack_name
+  }
+}
+
+
+resource "aws_iam_role_policy" "send_startup_email_lambda_policy" {
+  role = module.send_startup_email.lambda_role_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sns:Publish"
+        ],
+        Effect   = "Allow",
+        Resource = aws_sns_topic.status_updates.arn
+      }
+    ]
+  })
+}
+
