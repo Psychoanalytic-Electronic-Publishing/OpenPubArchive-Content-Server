1dd69fbb9832add07ef57537cfd92dc4ce7d4e43
nrshapiro <nrshapiro@gmail.com>
2023-06-06 17:03:05 -0400

    opasLoader 2023.0606/v2.1.041
    When opasLoader is about to exit normally it prints "Normal exit...using 255"

--- app/opasDataLoader/opasDataLoader.py
+++ app/opasDataLoader/opasDataLoader.py
@@ -7,7 +7,7 @@
 __author__      = "Neil R. Shapiro"
 __copyright__   = "Copyright 2023, Psychoanalytic Electronic Publishing"
 __license__     = "Apache 2.0"
-__version__     = "2023.0606/v2.1.040"
+__version__     = "2023.0606/v2.1.041"
 __status__      = "Development"
 
 # !!! IMPORTANT: Increment opasXMLProcessor version (if version chgd). It's written to the XML !!!
