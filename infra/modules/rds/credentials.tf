4c4979684237b4313a462ab5c21b9b93067c063a
jordanallen-dev <79658146+jordanallen-dev@users.noreply.github.com>
2023-06-25 23:36:38 +0100

    Configure RDS proxy (#224)
    * Allow ingress from self
    
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>
    
    * Provision RDS proxy
    
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>
    
    ---------
    
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

---
+++ infra/modules/rds/credentials.tf
@@ -0,0 +1,25 @@
+resource "aws_secretsmanager_secret" "credentials" {
+  name = "${var.stack_name}/rds-credentials/${var.env}"
+
+  tags = {
+    stack = var.stack_name
+    env   = var.env
+  }
+}
+
+locals {
+  credentials = {
+    username             = var.username
+    password             = var.password
+    engine               = "mysql"
+    host                 = aws_db_instance.mysql.address
+    port                 = aws_db_instance.mysql.port
+    dbname               = aws_db_instance.mysql.db_name
+    dbInstanceIdentifier = aws_db_instance.mysql.id
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "credentials" {
+  secret_id     = aws_secretsmanager_secret.credentials.id
+  secret_string = jsonencode(local.credentials)
+}
