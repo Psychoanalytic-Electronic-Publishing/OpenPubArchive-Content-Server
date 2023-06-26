fcd7bcb327f534ccb027a63cebca5d19a1d3eaa6
Jordan Allen <jordan@jordanallen.org>
2023-06-26 18:03:33 +0100

    Remove unnecessary connection pool config
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/modules/rds/proxy.tf
+++ infra/modules/rds/proxy.tf
@@ -40,10 +40,8 @@ resource "aws_db_proxy_default_target_group" "proxy_group" {
 
   connection_pool_config {
     connection_borrow_timeout    = 120
-    init_query                   = "SET x=1, y=2"
     max_connections_percent      = 100
     max_idle_connections_percent = 50
-    session_pinning_filters      = ["EXCLUDE_VARIABLE_SETS"]
   }
 }
 
