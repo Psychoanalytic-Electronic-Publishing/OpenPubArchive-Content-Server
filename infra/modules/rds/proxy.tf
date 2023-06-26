fcb4d241849f7f43a6fdc4926dccff11473f8602
jordanallen-dev <79658146+jordanallen-dev@users.noreply.github.com>
2023-06-26 16:59:39 +0100

    Use AWS provider for RDS proxy (#225)
    * Configure policy and target
    
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>
    
    * Filter out unsupported availability zone
    
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>
    
    ---------
    
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/modules/rds/proxy.tf
+++ infra/modules/rds/proxy.tf
@@ -1,25 +1,56 @@
-module "proxy" {
-  source = "terraform-aws-modules/rds-proxy/aws"
+data "aws_subnet" "private_subnet" {
+  for_each = toset(data.aws_subnets.private.ids)
+  id       = each.key
+}
 
+# RDS proxy does not support AZ3
+locals {
+  private_subnet_ids_filtered = [
+    for subnet in data.aws_subnet.private_subnet :
+    subnet.id if subnet.availability_zone_id != "use1-az3"
+  ]
+}
+
+resource "aws_db_proxy" "rds" {
   name                   = "${var.stack_name}-rds-proxy-${var.env}"
-  iam_role_name          = "${var.stack_name}-proxy-role-${var.env}"
-  vpc_subnet_ids         = data.aws_subnets.private.ids
-  vpc_security_group_ids = [aws_security_group.db.id]
-
-
-  auth = {
-    "root" = {
-      description = "MySQL root password"
-      secret_arn  = aws_secretsmanager_secret.credentials.arn
-    }
-  }
-
+  debug_logging          = false
   engine_family          = "MYSQL"
-  target_db_instance     = true
-  db_instance_identifier = aws_db_instance.mysql.id
+  idle_client_timeout    = 1800
+  require_tls            = false
+  role_arn               = aws_iam_role.proxy.arn
+  vpc_security_group_ids = [aws_security_group.db.id]
+  vpc_subnet_ids         = local.private_subnet_ids_filtered
+
+  auth {
+    auth_scheme = "SECRETS"
+    iam_auth    = "DISABLED"
+    secret_arn  = aws_secretsmanager_secret.credentials.arn
+  }
 
   tags = {
     stack = var.stack_name
     env   = var.env
   }
+}
+
+resource "aws_db_proxy_default_target_group" "proxy_group" {
+  depends_on = [aws_db_proxy.rds]
+
+  db_proxy_name = aws_db_proxy.rds.name
+
+  connection_pool_config {
+    connection_borrow_timeout    = 120
+    init_query                   = "SET x=1, y=2"
+    max_connections_percent      = 100
+    max_idle_connections_percent = 50
+    session_pinning_filters      = ["EXCLUDE_VARIABLE_SETS"]
+  }
+}
+
+resource "aws_db_proxy_target" "proxy_target" {
+  depends_on = [aws_db_proxy_default_target_group.proxy_group]
+
+  db_instance_identifier = aws_db_instance.mysql.id
+  db_proxy_name          = aws_db_proxy.rds.name
+  target_group_name      = aws_db_proxy_default_target_group.proxy_group.name
 }
