d55b934f4033b41401c266632a509e7008efd223
Jordan Allen <jordan@jordanallen.org>
2023-06-29 16:41:14 +0100

    Update error email copy
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- dataUtility/email/error.py
+++ dataUtility/email/error.py
@@ -9,11 +9,13 @@ stepfunctions_url = "https://us-east-1.console.aws.amazon.com/states/home?region
 def handler(event, context):
     print("Event: ", event)
 
-    msg = "Task execution failed \n\n"
+    msg = "Job execution failed \n\n"
     msg += f"Execution details: {stepfunctions_url}{event['executionArn']} \n\n"
+    msg += f"Utility: {event['task']['utility']}\n"
+    msg += f"Arguments: {event['task']['args']}\n"
 
     subject = f"Data Utility: {event['executionArn'].split(':')[-1]}"
-
+    
     sns.publish(
         TargetArn=os.environ["SNS_TOPIC_ARN"],
         Message=msg,
