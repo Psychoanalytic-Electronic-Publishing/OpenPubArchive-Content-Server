2c3f3ffd8c289c94f6f5800a6e09d556d925ebe9
Jordan Allen <jordan@jordanallen.org>
2023-07-03 16:57:01 +0100

    Configure EBSCO bucket policy
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

---
+++ infra/shared/s3.tf
@@ -0,0 +1,81 @@
+resource "aws_s3_bucket_policy" "pep_web_live_data" {
+  bucket = data.aws_s3_bucket.pep_web_live_data.id
+  policy = data.aws_iam_policy_document.allow_datasync_from_another_account.json
+}
+
+data "aws_iam_policy_document" "allow_datasync_from_another_account" {
+  statement {
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::872344130825:role/refarch/RoleCcpDataSync",
+        "arn:aws:iam::914367642226:role/refarch/RoleCcpDataSync"
+      ]
+    }
+
+    actions = [
+      "s3:GetBucketLocation",
+      "s3:ListBucketMultipartUploads",
+    ]
+
+    resources = [
+      "${data.aws_s3_bucket.pep_web_live_data.arn}"
+    ]
+  }
+
+  statement {
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::872344130825:role/refarch/RoleCcpDataSync",
+        "arn:aws:iam::914367642226:role/refarch/RoleCcpDataSync"
+      ]
+    }
+
+    actions = [
+      "s3:ListBucket"
+    ]
+
+    resources = [
+      "${data.aws_s3_bucket.pep_web_live_data.arn}"
+    ]
+
+    condition {
+      test     = "StringLike"
+      variable = "s3:prefix"
+
+      values = [
+        "",
+        "_PEPArchive/*",
+        "_PEPFree/*",
+        "graphics/*",
+      ]
+    }
+  }
+
+  statement {
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::872344130825:role/refarch/RoleCcpDataSync",
+        "arn:aws:iam::914367642226:role/refarch/RoleCcpDataSync"
+      ]
+    }
+
+    actions = [
+      "s3:PutObjectTagging",
+      "s3:ListMultipartUploadParts",
+      "s3:GetObjectTagging",
+      "s3:GetObject",
+      "s3:AbortMultipartUpload",
+      "s3:GetObjectVersion",
+      "s3:GetObjectVersionTagging"
+    ]
+
+    resources = [
+      "${data.aws_s3_bucket.pep_web_live_data.arn}/_PEPFree/*(bEXP_ARCH1).xml",
+      "${data.aws_s3_bucket.pep_web_live_data.arn}/_PEPArchive/*(bEXP_ARCH1).xml",
+      "${data.aws_s3_bucket.pep_web_live_data.arn}/graphics/*(bEXP_ARCH1).xml",
+    ]
+  }
+}
