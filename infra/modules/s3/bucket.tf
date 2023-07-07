807cd15d53e8a0a055742a04c6dd98c6c96a9354
Jordan Allen <jordan@jordanallen.org>
2023-07-07 16:52:40 +0100

    Add Bucket tag
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/modules/s3/bucket.tf
+++ infra/modules/s3/bucket.tf
@@ -2,7 +2,8 @@ resource "aws_s3_bucket" "pep_web_data" {
   bucket = var.bucket_name
 
   tags = {
-    stack = var.stack_name
-    env   = var.env
+    Bucket = var.bucket_name
+    stack  = var.stack_name
+    env    = var.env
   }
 }
