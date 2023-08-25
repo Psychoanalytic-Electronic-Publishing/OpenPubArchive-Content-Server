a92d506c49eb95b6aa0b15ab33c4654008ff6af6
Jordan Allen <jordan@jordanallen.org>
2023-08-25 15:34:02 +0100

    Update resource names
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/modules/data-utility-cron/stats.tf
+++ infra/modules/data-utility-cron/stats.tf
@@ -1,15 +1,15 @@
-resource "aws_cloudwatch_event_rule" "weekly_site_map" {
+resource "aws_cloudwatch_event_rule" "weekly_stat_update" {
   count = var.env == "production" ? 1 : 0
 
-  name                = "${var.stack_name}-weekly-site-map-${var.env}"
+  name                = "${var.stack_name}-weekly-stat-update-${var.env}"
   description         = "Run stat updater every Sunday at 15:00 UTC"
   schedule_expression = "cron(0 15 ? * SUN *)"
 }
 
-resource "aws_cloudwatch_event_target" "weekly_site_map_target" {
+resource "aws_cloudwatch_event_target" "weekly_stat_update_target" {
   count = var.env == "production" ? 1 : 0
 
-  rule     = aws_cloudwatch_event_rule.weekly_site_map[0].name
+  rule     = aws_cloudwatch_event_rule.weekly_stat_update[0].name
   role_arn = aws_iam_role.allow_cloudwatch_to_execute_role.arn
   arn      = var.state_machine_arn
   input = jsonencode([
