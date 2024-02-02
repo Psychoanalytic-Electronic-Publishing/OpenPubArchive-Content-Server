99f84536d0a55ebdd2d9029d6302e769d30cce3a
Jordan Allen <jordan@jordanallen.org>
2024-02-02 14:52:02 +0000

    Only run weekly linker on production
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/modules/data-utility-cron/linker.tf
+++ infra/modules/data-utility-cron/linker.tf
@@ -1,4 +1,6 @@
 resource "aws_cloudwatch_event_rule" "weekly_data_linker" {
+  count = var.env == "production" ? 1 : 0
+
   name                = "${var.stack_name}-weekly-data-linker-${var.env}"
   description         = "Run ${var.env} state machine every thursday at 03:00 UTC"
   schedule_expression = "cron(0 3 ? * 5 *)"
