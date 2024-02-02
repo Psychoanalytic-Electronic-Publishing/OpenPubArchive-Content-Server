be7891ef298f452a9c188700740b0918b9445e43
Jordan Allen <jordan@jordanallen.org>
2024-02-02 14:51:30 +0000

    Run data linker every thursday at 03:00 UTC
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

---
+++ infra/modules/data-utility-cron/linker.tf
@@ -0,0 +1,24 @@
+resource "aws_cloudwatch_event_rule" "weekly_data_linker" {
+  name                = "${var.stack_name}-weekly-data-linker-${var.env}"
+  description         = "Run ${var.env} state machine every thursday at 03:00 UTC"
+  schedule_expression = "cron(0 3 ? * 5 *)"
+}
+
+resource "aws_cloudwatch_event_target" "nightly_data_pipeline_target" {
+  rule     = aws_cloudwatch_event_rule.nightly_data_pipeline.name
+  role_arn = aws_iam_role.allow_cloudwatch_to_execute_role.arn
+  arn      = var.state_machine_arn
+  input = jsonencode({
+    "task" : [
+      [
+        {
+          "limitRam" : false,
+          "directory" : "opasDataLoader",
+          "utility" : "opasDataLinker",
+          "args" : "--verbose --key ^*.* --unlinked"
+        }
+      ]
+    ]
+  })
+}
+
