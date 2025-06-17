04ceaa3201f2f76b6f4f0410593c986211c9b76d
Jordan Allen <jordan@jordanallen.org>
2025-06-17 19:50:12 +0100

    Update ECR lifecycle policy to retain last 10 tagged images

--- infra/modules/ecr/ecr.tf
+++ infra/modules/ecr/ecr.tf
@@ -15,12 +15,24 @@ resource "aws_ecr_lifecycle_policy" "opas_policy" {
     "rules": [
         {
             "rulePriority": 1,
-            "description": "Expire images older than 14 days",
+            "description": "Keep last 10 tagged images",
+            "selection": {
+                "tagStatus": "tagged",
+                "countType": "imageCountMoreThan",
+                "countNumber": 10
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 2,
+            "description": "Expire untagged images older than 1 day",
             "selection": {
                 "tagStatus": "untagged",
                 "countType": "sinceImagePushed",
                 "countUnit": "days",
-                "countNumber": 14
+                "countNumber": 1
             },
             "action": {
                 "type": "expire"
