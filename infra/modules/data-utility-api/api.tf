569a6cd9aee11e73bcfa248b0904819130d448e3
Jordan Allen <jordan@jordanallen.org>
2025-06-18 21:18:33 +0100

    Revert API Gateway change
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- infra/modules/data-utility-api/api.tf
+++ infra/modules/data-utility-api/api.tf
@@ -44,10 +44,6 @@ resource "aws_api_gateway_deployment" "api_deployment" {
   lifecycle {
     create_before_destroy = true
   }
-}
 
-resource "aws_api_gateway_stage" "api_stage" {
-  stage_name    = "v1"
-  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
-  deployment_id = aws_api_gateway_deployment.api_deployment.id
+  stage_name = "v1"
 }
