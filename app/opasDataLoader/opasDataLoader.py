d48cbc2a6c53d4468006381735228045c9f974ea
Jordan Allen <jordan@jordanallen.org>
2025-06-20 18:57:18 +0100

    Test Data Utility change
    Signed-off-by: Jordan Allen <jordan@jordanallen.org>

--- app/opasDataLoader/opasDataLoader.py
+++ app/opasDataLoader/opasDataLoader.py
@@ -499,6 +499,7 @@ def file_is_same_as_in_solr(solrcore, filename, timestamp_str):
 
 #------------------------------------------------------------------------------------------------------
 def main():
+    # Test change
     
     global options  # so the information can be used in support functions
     
